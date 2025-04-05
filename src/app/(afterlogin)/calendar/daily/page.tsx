e6e6999d8eb2ae8940bf42584d74ac804c931821
jihye <wlgul817@gmail.com>
2025-04-05 16:53:31 +0900

    TTOL-11 style: 화면 너비에 맞춰 div의 너비가 변경되도록 수정

--- src/app/(afterlogin)/calendar/daily/page.tsx
+++ src/app/(afterlogin)/calendar/daily/page.tsx
@@ -105,75 +105,73 @@ export default function Daily() {
               <span className='whitespace-nowrap'>{`${finishedTaskCount} / ${mockData.length}`}</span>
             </div>
           </BoardTitle>
-          <div className='flex h-full bg-[#FAFAFA] px-[32px] py-[24px]'>
-            <div className='flex gap-[34px]'>
-              <div className='min-w-[752px]'>
-                <div className='mb-[18px] flex justify-between'>
-                  <span className='text-[24px] font-semibold'>Todos</span>
-                  <button className='bg-primary-400 hover:bg-primary-500 h-[40px] w-[40px] cursor-pointer rounded-[10px] bg-[url(/assets/plus-small.png)] bg-center bg-no-repeat'></button>
-                </div>
-                <ul className='flex flex-col gap-y-[10px]'>
-                  {mockData.map((task, index) => {
-                    return (
-                      <TaskListItem
-                        task={task}
-                        index={index}
-                        key={task.task_id}
-                        handleCheckClick={handleCheckClick}
-                      />
-                    );
-                  })}
-                </ul>
+          <div className='flex h-full justify-between bg-[#FAFAFA] px-[32px] py-[24px]'>
+            <div className='min-w-[752px] mr-[34px]'>
+              <div className='mb-[18px] flex justify-between'>
+                <span className='text-[24px] font-semibold'>Todos</span>
+                <button className='bg-primary-400 hover:bg-primary-500 h-[40px] w-[40px] cursor-pointer rounded-[10px] bg-[url(/assets/plus-small.png)] bg-center bg-no-repeat'></button>
               </div>
-              <div className='min-w-[400px]'>
-                <div className='mb-[18px]'>
-                  <span className='text-[24px] font-semibold'>Next moves</span>
-                </div>
-                <div className='flex flex-col gap-[18px]'>
-                  {pendingTask.map((task, index) => {
-                    return (
-                      <div
-                        key={task.task_id}
-                        className='bg-primary-0 border-primary-200 w-full rounded-[10px] border-1 px-[64px] py-[23px]'
-                      >
-                        <div className='mb-[10px] flex items-center justify-between'>
-                          <div>
-                            <span className='mr-[10px] text-[20px] font-bold'>
-                              출발
-                            </span>
-                            <span className='text-[20px] font-semibold'>
-                              {mockData[0].task_id === task.task_id
-                                ? 'House'
-                                : index === 0
-                                  ? mockData[task.task_id - 2].place_name
-                                  : pendingTask[index - 1].place_name}
-                            </span>
-                          </div>
-                          <span>{formatTime(task.start_time)}</span>
+              <ul className='flex flex-col gap-y-[10px]'>
+                {mockData.map((task, index) => {
+                  return (
+                    <TaskListItem
+                      task={task}
+                      index={index}
+                      key={task.task_id}
+                      handleCheckClick={handleCheckClick}
+                    />
+                  );
+                })}
+              </ul>
+            </div>
+            <div className='min-w-[400px]'>
+              <div className='mb-[18px]'>
+                <span className='text-[24px] font-semibold'>Next moves</span>
+              </div>
+              <div className='flex flex-col gap-[18px]'>
+                {pendingTask.map((task, index) => {
+                  return (
+                    <div
+                      key={task.task_id}
+                      className='bg-primary-0 border-primary-200 w-full rounded-[10px] border-1 px-[64px] py-[23px]'
+                    >
+                      <div className='mb-[10px] flex items-center justify-between'>
+                        <div>
+                          <span className='mr-[10px] text-[20px] font-bold'>
+                            출발
+                          </span>
+                          <span className='text-[20px] font-semibold'>
+                            {mockData[0].task_id === task.task_id
+                              ? 'House'
+                              : index === 0
+                                ? mockData[task.task_id - 2].place_name
+                                : pendingTask[index - 1].place_name}
+                          </span>
                         </div>
-                        <div className='flex items-center justify-between'>
-                          <div>
-                            <span className='mr-[10px] text-[20px] font-bold'>
-                              도착
-                            </span>
-                            <span className='text-[20px] font-semibold'>
-                              {task.place_name}
-                            </span>
-                          </div>
-                          <span>{formatTime(task.start_time)}</span>
-                        </div>
-                        <div className='bg-primary-0 border-primary-200 mt-[20px] flex h-[180px] items-center justify-center rounded-[10px] border-1'>
-                          <Image
-                            src='/assets/map.png'
-                            width={40}
-                            height={40}
-                            alt='map icon'
-                          />
-                        </div>
+                        <span>{formatTime(task.start_time)}</span>
                       </div>
-                    );
-                  })}
-                </div>
+                      <div className='flex items-center justify-between'>
+                        <div>
+                          <span className='mr-[10px] text-[20px] font-bold'>
+                            도착
+                          </span>
+                          <span className='text-[20px] font-semibold'>
+                            {task.place_name}
+                          </span>
+                        </div>
+                        <span>{formatTime(task.start_time)}</span>
+                      </div>
+                      <div className='bg-primary-0 border-primary-200 mt-[20px] flex h-[180px] items-center justify-center rounded-[10px] border-1'>
+                        <Image
+                          src='/assets/map.png'
+                          width={40}
+                          height={40}
+                          alt='map icon'
+                        />
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </div>
