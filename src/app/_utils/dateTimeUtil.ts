a358defafd0c098769c337e1d8bf8b51bf94d2cf
rkdcodus <rcd7275@naver.com>
2025-04-15 01:16:04 +0900

    TTOL-47 refactor: date 및 time 관련 유틸 함수 파일 분리
    - 기존에 index.ts에 있던 formatTime 함수 또한 dateTimeUtil 파일로 이동
    - DayPickerModal에서 사용되는 parseTimeString, applyTimeToDate 함수 생성
    - index.ts 삭제

---
+++ src/app/_utils/dateTimeUtil.ts
@@ -0,0 +1,19 @@
+import { setHours, setMinutes } from 'date-fns';
+
+export const formatTime = (date: string | Date) => {
+  return new Intl.DateTimeFormat('en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  }).format(new Date(date));
+};
+
+export const parseTimeString = (time: string): [number, number] => {
+  const [hours, minutes] = time.split(':').map(str => parseInt(str, 10));
+  return [hours, minutes];
+};
+
+export const applyTimeToDate = (date: Date, time: string) => {
+  const [hours, minutes] = parseTimeString(time);
+  return setHours(setMinutes(date, minutes), hours);
+};
--- src/app/_utils/index.ts
+++
@@ -1,9 +0,0 @@
-const formatTime = (date: string | Date) => {
-  return new Intl.DateTimeFormat('en-US', {
-    hour: 'numeric',
-    minute: '2-digit',
-    hour12: true,
-  }).format(new Date(date));
-};
-
-export { formatTime };
