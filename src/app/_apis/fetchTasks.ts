b2bec4d9261fe38b03ac915dc203eb29027f8b0e
jihye <wlgul817@gmail.com>
2025-04-16 17:39:34 +0900

    TTOL-53 feat: 일간 일정 api 연동
    일간 일정 api 추가 및 연동, mock 데이터 삭제

--- src/app/_apis/fetchTasks.ts
+++ src/app/_apis/fetchTasks.ts
@@ -22,10 +22,29 @@ export const fetchDailyTask = async (date: Date): Promise<TaskPayload[]> => {
   }
 };
 
+export const fetchWeeklyTask = async (date: Date): Promise<TaskPayload[]> => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+  const firstDayOfWeek = firstDay.getDay();
+  const week = Math.ceil((date.getDate() + firstDayOfWeek) / 7);
+
+  try {
+    const response = await api.get('/tasks/week', {
+      params: { year, month, week },
+    });
+
+    return response.data.data;
+  } catch (error) {
+    console.error('API 호출 중 에러가 발생했습니다.', error);
+    return [];
+  }
+};
+
 export const fetchMonthlyTask = async (date: Date): Promise<TaskPayload[]> => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
-  
+
   try {
     const response = await api.get('/tasks/month', {
       params: { year, month },
@@ -36,4 +55,4 @@ export const fetchMonthlyTask = async (date: Date): Promise<TaskPayload[]> => {
     console.error('API 호출 중 에러가 발생했습니다.', error);
     return [];
   }
-}
+};
