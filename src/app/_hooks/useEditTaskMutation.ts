8944bf434ec03ef1d7f821eef5d44686b3275187
rkdcodus <rcd7275@naver.com>
2025-04-18 13:45:27 +0900

    TTOL-54 feat: 일정 useMutation 성공 시 get 요청 실행 코드 세팅

--- src/app/_hooks/useEditTaskMutation.ts
+++ src/app/_hooks/useEditTaskMutation.ts
@@ -1,11 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { patchTask } from '../_apis/tasks';
 
 const useEditTaskMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationKey: ['editTask'],
     mutationFn: patchTask,
     onError: error => console.error('일정 수정 실패:', error),
+    onSuccess: () => {
+      // TODO: useQuery 생성 시 invalidateQueries로 쿼리 무효화 => get 요청 다시 실행
+      queryClient.invalidateQueries({ queryKey: [''] });
+    },
   });
 };
 
