2aa67588d39555059c2083066b4870657c8cfddd
rkdcodus <rcd7275@naver.com>
2025-04-29 01:06:32 +0900

    TTOL-72 refactor: WeatherLocationSetting 컴포넌트에 변경된 NormalInput 적용

--- src/app/(afterlogin)/setting/_settings/WeatherLocationSetting.tsx
+++ src/app/(afterlogin)/setting/_settings/WeatherLocationSetting.tsx
@@ -5,16 +5,16 @@ function WeatherLocationSetting() {
   return (
     <div className='flex'>
       <NormalInput
-        value={''}
         className='mr-[10px] max-w-[400px] flex-grow text-[14px] sm:text-[16px]'
-      >
-        <Image
-          src='/assets/location-line.png'
-          alt='calendar icon'
-          width={24}
-          height={24}
-        />
-      </NormalInput>
+        leftIcon={
+          <Image
+            src='/assets/location-line.png'
+            alt='calendar icon'
+            width={24}
+            height={24}
+          />
+        }
+      />
     </div>
   );
 }
